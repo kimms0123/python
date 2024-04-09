42e4cf6fdd801b7a892eba505a3472e03bdcb21f
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2024-04-09 21:41:22 +0900

    Add files via upload

---
+++ day_01/input.py
@@ -0,0 +1,21 @@
+#print: 글 나오도록 하는 기능
+#input: 글을 입력 하는 기능
+# drama = input("당신이 최애 드라마는?")
+# print(f"최애 드라마는 {drama}이군요!")
+#
+# name = input("당신의 이름은?")
+# print(f"당신의 이름은 {name}이군요!")
+
+# 드래그+ctrl+/: 드래그 한 범위 주석 처리
+
+#자기 소개 프로그램
+name = input("당신의 이름은?>>")
+age = input("당신의 나이는?>>")
+color = input("당신이 좋아하는 색은?>>")
+
+print(f"제 이름은 {name}이고, 나이는 {age}살, 좋아하는 색상은 {color}입니다!")
+
+#간단한 일정 안내 프로그램
+today_date = input("오늘의 날짜를 입력해주세요(년.월.일)>>")
+schedule = input("계획하고 있는일정을 입력해주세요>>")
+print(f"오늘은 {today_date}, 계획하신 일정은{schedule}입니다.")

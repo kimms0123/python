1af688404e2d2a9f93f44228f0bce8bc26f424d2
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2024-04-11 21:50:47 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+#review Quiz
+# 어제의 일기
+lastDate = input("날짜를 입력해주세요>>")
+case = input("하루 동안 있었던 주요 사건을 적어주세요>>")
+
+print(f"어제는 {lastDate}, 주요 사건은 {case}였습니다!")
+
+#2 아침, 점심, 저녁 먹은 메뉴 보여주기
+date = input("날짜를 입력해주세요>>")
+moring = input("아침 메뉴>>")
+noon = input("점심 메뉴>>")
+night = input("저녁 메뉴>>")
+
+print(f"오늘은 {date}입니다. 아침에는 {moring}, 점심에는 {noon}, 저녁에는 {night} 먹었습니다.")
+
+
